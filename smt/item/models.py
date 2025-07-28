aa1e8ed3d79611d0542753f5efe0e42c38fe9003
Zevobla <ghost-brake-your-home@protonmail.com>
2025-07-28 14:51:04 +0300

    Define Item model with name, description, and price fields

--- smt/item/models.py
+++ smt/item/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(verbose_name="Item's name", max_length=256, blank=False)
+    description = models.TextField(verbose_name="Item's description", max_length=4096, blank=False)
+    price = models.DecimalField(verbose_name="Item's price", max_digits=10, decimal_places=2, blank=False)
