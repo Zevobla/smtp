e48a1ae117764e839741dbc6443162c11d6993f2
Zevobla <ghost-brake-your-home@protonmail.com>
2025-07-28 14:55:42 +0300

    Add __str__ method to Item model for better string representation

--- smt/item/models.py
+++ smt/item/models.py
@@ -4,3 +4,6 @@ class Item(models.Model):
     name = models.CharField(verbose_name="Item's name", max_length=256, blank=False)
     description = models.TextField(verbose_name="Item's description", max_length=4096, blank=False)
     price = models.DecimalField(verbose_name="Item's price", max_digits=10, decimal_places=2, blank=False)
+    def __str__(self):
+        return self.name
+
