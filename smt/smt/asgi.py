8e62b4602a83ba8484ae8b59489bea55267d8d9d
Zevobla <ghost-brake-your-home@protonmail.com>
2025-08-03 23:23:21 +0300

    Add uvicorn to requirements and configure ASGI application entry point

--- smt/smt/asgi.py
+++ smt/smt/asgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import uvicorn
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smt.settings')
 
 application = get_asgi_application()
+
+if __name__ == "__main__":
+    uvicorn.run("smt.asgi:application", host="0.0.0.0", port=8000, log_level="info")
