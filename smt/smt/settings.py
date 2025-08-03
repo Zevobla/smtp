dce2217d21d2abaa2defa6a8a76f16dae24f7f3a
Zevobla <ghost-brake-your-home@protonmail.com>
2025-08-04 00:01:26 +0300

    Add CSRF_TRUSTED_ORIGINS setting for enhanced security

--- smt/smt/settings.py
+++ smt/smt/settings.py
@@ -132,3 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add trusted origins for CSRF protection
+CSRF_TRUSTED_ORIGINS = [BASE_URL]
